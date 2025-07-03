5f60e075bbc02db9d11f701e5217fd8b4a8b8a24
Pranesh <94618543+pranesh2104@users.noreply.github.com>
2025-07-03 12:15:05 +0530

    final changes

---
+++ api/index.js
@@ -0,0 +1,12 @@
+import('../dist/todo/server/server.mjs')
+  .then(module => module.default)
+  .catch(error => {
+    console.error('Failed to load server module:', error);
+    throw error;
+  });
+
+
+export default async (req, res) => {
+  const { default: app } = await import('../dist/YOUR-APP/server/server.mjs');
+  return app(req, res);
+};
