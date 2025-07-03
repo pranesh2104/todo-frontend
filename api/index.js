9f528d2b1cf7ae24d248d89c8ab486274fe65bcd
Pranesh <94618543+pranesh2104@users.noreply.github.com>
2025-07-03 13:43:30 +0530

    cookie check

--- api/index.js
+++ api/index.js
@@ -8,5 +8,16 @@ import('../dist/todo/server/server.mjs')
 
 export default async (req, res) => {
   const { default: app } = await import('../dist/todo/server/server.mjs');
+
+  const originalSetHeader = res.setHeader;
+
+  // Intercept headers to ensure cookies are set
+  res.setHeader = function (name, value) {
+    if (name.toLowerCase() === 'set-cookie') {
+      console.log('Setting cookie:', value);
+    }
+    return originalSetHeader.call(this, name, value);
+  };
+
   return app(req, res);
 };
